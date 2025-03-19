5216b1307af8d81a4acd464c0ca2014a6b3e73ad
leylamaharramli <leyla@example.com>
2025-03-19 20:49:56 +0400

    first commit

---
+++ AccessModifiers/Singer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class Singer
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Age { get; set; }
+
+
+
+        public Singer(string name, string surname, string age) 
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+
+        public void Limit(string name, string surname, string age)
+        {
+            if(name.Length > 100 || surname.Length > 100) 
+            {
+                Console.WriteLine("Ad ve ya Soyadin limiti 100u ashib");
+            }
+
+            if(age.Length > 100)
+            {
+                Console.WriteLine("Yasin limiti 100u ashib");
+            }
+
+        }
+    }
+}
